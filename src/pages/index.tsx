018d882a5642f99ca098d1c965b13d2e2bbea99d
Wisteria30 <skymars0327@yahoo.co.jp>
2022-09-18 15:56:19 +0900

    fix home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import DocumentHead from '../components/document-head'
+import ExtLink from '../components/ext-link'
 import styles from '../styles/page.module.css'
 
 const RenderPage = () => (
@@ -7,7 +8,7 @@ const RenderPage = () => (
 
     <div>
       <h2>ブログです。</h2>
-      <p>私が通った道を書いていくことで知の高速道路を目指します。</p>
+      <p>私、<ExtLink href="https://twitter.com/kakka_q">かっか</ExtLink>が通った道を書いていくことで知の高速道路を目指します。</p>
     </div>
   </div>
 )
